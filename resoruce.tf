90b841e2d7f9b5d8b69ba2d592881bfe58d2f0c0
kurtuluş yılmaz <kurtulusyilmaz@kurtuluss-MacBook-Pro.local>
2022-01-03 23:56:35 +0000

    terraform initialized

---
+++ resoruce.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("silent-octagon-336420-1dd44d3f7a7d.json")
+
+  project = "silent-octagon-336420"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
