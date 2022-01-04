4e9c998461c208bca72ef0356f2920e8a7127ebb
kurtuluş yılmaz <kurtulusyilmaz@kurtuluss-MacBook-Pro.local>
2022-01-04 00:37:17 +0000

    asdded new resource

--- resoruce.tf
+++ resoruce.tf
@@ -18,3 +18,7 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_network" "vpc_network2" {
+  name = "terraform-network"
+}
