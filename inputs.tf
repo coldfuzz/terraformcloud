7426debbcdbb56880611febee4799f46c301c4ac
kurtuluş yılmaz <kurtulusyilmaz@kurtuluss-MacBook-Pro.local>
2022-01-04 09:46:02 +0000

    added env variables

---
+++ inputs.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
+variable "project" {
+  default = "silent-octagon-336420"
+}
+// bionic-octane-337209
+variable "credential_path" {
+  default = "silent-octagon-336420-1dd44d3f7a7d.json"
+}
